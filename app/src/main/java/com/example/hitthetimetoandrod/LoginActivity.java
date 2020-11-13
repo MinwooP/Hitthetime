3bab7230216f213a3b5c0062e27f0064c95b35fa
박민우 <31370590+MinwooP@users.noreply.github.com>
2020-11-13 18:21:53 +0900

    initial commit - first upload hitthetime android project.

---
+++ app/src/main/java/com/example/hitthetimetoandrod/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.hitthetimetoandrod;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Intent intent = getIntent();
+    }
+}
---
+++ app/src/main/java/com/example/hitthetimetoandrod/LoginActivity_sec.java
@@ -0,0 +1,14 @@
+package com.example.hitthetimetoandrod;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class LoginActivity_sec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sec);
+    }
+}
