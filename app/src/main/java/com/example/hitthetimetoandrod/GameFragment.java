20e0557a95d78a4debf3366611b44efc3a747bdc
Eunno An <12171647@inha.edu>
2020-12-14 01:11:11 +0900

    Eunno Commit

--- app/src/main/java/com/example/hitthetimetoandrod/GameFragment.java
+++ app/src/main/java/com/example/hitthetimetoandrod/GameFragment.java
@@ -1,16 +1,28 @@
 package com.example.hitthetimetoandrod;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
 import android.os.SystemClock;
+import android.renderscript.Sampler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.airbnb.lottie.Lottie;
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
 
 import org.w3c.dom.Text;
 
@@ -25,14 +37,7 @@ public class GameFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private Button btn_Start, btn_Stop, btn_Reset;
-    private TextView text_Goal, text_Now;
-    private long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L;
-    Handler handler;
-    int Seconds, Minutes, MilliSeconds;
-    private Thread timeThread = null;
-    private Boolean isRunning = true;
-
+    private Button btn_gameStart;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -66,7 +71,7 @@ public class GameFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        handler = new Handler();
+
     }
 
     @Override
@@ -74,58 +79,18 @@ public class GameFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_game, container, false);
-        btn_Start = (Button) v.findViewById(R.id.btn_start);
-        btn_Stop = (Button) v.findViewById(R.id.btn_stop);
-        btn_Reset = (Button) v.findViewById(R.id.btn_reset);
-        text_Goal = (TextView) v.findViewById(R.id.text_targetTime);
-        text_Now = (TextView) v.findViewById(R.id.text_nowTime);
-        btn_Start.setOnClickListener(new View.OnClickListener() {
+        btn_gameStart = (Button)v.findViewById(R.id.startButton);
+        btn_gameStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StartTime = SystemClock.uptimeMillis();
-                handler.postDelayed(runnable, 0);
-                btn_Reset.setEnabled(false);
-                btn_Start.setEnabled(false);
-            }
-        });
-
-        btn_Reset.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                MillisecondTime = 0L;
-                StartTime = 0L;
-                TimeBuff = 0L;
-                UpdateTime = 0L;
-                Seconds = 0;
-                Minutes = 0;
-                MilliSeconds = 0;
-
-                text_Now.setText("00:00:00");
-                btn_Start.setEnabled(true);
-            }
-        });
-        btn_Stop.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                handler.removeCallbacks(runnable);
-                btn_Reset.setEnabled(true);
+                Intent intent = new Intent(getActivity(), TimeAttackActivity.class);
+                startActivity(intent);
             }
         });
         return v;
     }
 
-    public Runnable runnable = new Runnable(){
-        public void run(){
-            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
-            UpdateTime = TimeBuff + MillisecondTime;
-            Seconds = (int) (UpdateTime / 1000);
-            Minutes = Seconds / 60;
-            Seconds = Seconds%60;
-            MilliSeconds = (int) (UpdateTime % 1000);
-            text_Now.setText("" + Minutes + ":"
-                    + String.format("%02d", Seconds) + ":"
-                    + String.format("%03d", MilliSeconds));
-            handler.postDelayed(this, 0);
-        }
-    };
+
+
+
 }
