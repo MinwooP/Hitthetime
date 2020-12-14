ed81f029cab66a3bc1598474bad03c699f2f8acb
박민우 <31370590+MinwooP@users.noreply.github.com>
2020-12-14 23:53:10 +0900

    Add source about RecyclerView & Rank fragment, design UI

---
+++ app/src/main/java/com/example/hitthetimetoandrod/RecyclerItem.java
@@ -0,0 +1,27 @@
+package com.example.hitthetimetoandrod;
+
+public class RecyclerItem {
+    private String Rank;
+    private String name;
+    private String record;
+
+    public void setRank(String _rank) {
+        Rank = _rank ;
+    }
+    public void setName(String _title) {
+        name = _title ;
+    }
+    public void setRecord(String _record) {
+        record = _record ;
+    }
+
+    public String getRank() {
+        return this.Rank;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String  getRecord() {
+        return this.record;
+    }
+}
